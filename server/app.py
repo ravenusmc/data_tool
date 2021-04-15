588a47f049aa45391271efd5a440f268964259d6
ravenusmc <mcuddy77@gmail.com>
2021-04-15 08:41:26 -0400

    setting up initial esrver file

---
+++ server/app.py
@@ -0,0 +1,28 @@
+# libraries for use in project
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import datetime
+
+# importing code that I wrote
+# from data import *
+
+# configuration
+DEBUG = True
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app)
+
+# This route will get the data for the EVA Count Chart
+@app.route('/getEvaCountData', methods=['GET', 'POST'])
+def route_EVA_count_data():
+    # if request.method == 'POST':
+    #     data_obj = Data()
+    #     eva_count_data = data_obj.eva_count_by_year()
+    #     return jsonify(eva_count_data)
+
+if __name__ == '__main__':
+    app.run()
