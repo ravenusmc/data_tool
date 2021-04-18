f2aa2d7489c678f605f333a523b4a02ae2688956
ravenusmc <mcuddy77@gmail.com>
2021-04-18 11:50:52 -0400

    added about page

---
+++ client/src/components/generic/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- BootStrap Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light backgroundColor">
+      <a class="navbar-brand fontColor font" href="#">Data Tool</a>
+      <button
+        class="navbar-toggler"
+        style="color: #fff"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#"
+              ><router-link class="fontColor font" to="/">Home</router-link></a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"
+              ><router-link class="fontColor font" to="/about"
+                >About</router-link
+              ></a
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- End BootStrap Navbar -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.backgroundColor {
+  background-color: #041e42 !important;
+}
+.fontColor {
+  color: white !important;
+}
+.navbar-toggler {
+  background-color: white;
+}
+</style>
