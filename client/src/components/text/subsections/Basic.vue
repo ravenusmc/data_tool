64b398e4d740d91f1167661bde9d3b72f26af419
ravenusmc <mcuddy77@gmail.com>
2021-05-26 08:35:44 -0400

    working on text gauge

--- client/src/components/text/subsections/Basic.vue
+++ client/src/components/text/subsections/Basic.vue
@@ -1,32 +1,49 @@
 <template>
   <div>
     <section>
-			<h4><span class='make-bold'>Title: </span>{{ this.textFile.file_name }}</h4>
-			<h4><span class='make-bold'>Overall Sentiment: </span>{{ this.textFile.sentiment_speech_average }}</h4>
+      <h4>
+        <span class="make-bold">Title: </span>{{ this.textFile.file_name }}
+      </h4>
+      <h4>
+        <span class="make-bold">Overall Sentiment: </span>
+        <p class="sentiment-color">
+          {{ this.textFile.sentiment_speech_average }}
+        </p>
+      </h4>
       <div>
-        <p>The Overall Sentiment of the speech is: </p>
+        <p>The Overall Sentiment of the speech is:</p>
       </div>
+      <Guage percentage="33" />
     </section>
   </div>
 </template>
 
 <script>
+import Guage from "@/components/text/subsections/Gauge.vue";
+
 export default {
   name: "Basic",
+  components: {
+    Guage,
+  },
   props: {
     textFile: Object,
-  }
+  },
+  data() {
+    return {
+      sentimentClass: '',
+    };
+  },
 };
 </script>
 
 <style scoped>
-
 section {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   margin-top: 50px;
-  border: 2px solid red;
+  /* border: 2px solid red; */
 }
 </style>
---
+++ client/src/components/text/subsections/Gauge.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div></div>
+    <span>{{ percentage }}%</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Guage",
+  props: ["percentage"],
+  computed: {
+    rotate() {
+      const v = (this.percentage * 180) / 100;
+      return `rotate(${v}deg)`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* body { 
+  background-color: #4d4d4d; 
+} */
+
+/* @gauge-width: 400px;
+@gauge-heigth: @gauge-width / 2;
+@gauge-bar-width: 75px;
+@middle-width: @gauge-width - (@gauge-bar-width * 2);
+
+@gauge-color: #5df086;
+@gauge-bg-color: lighten(@gauge-color, 15%); */
+
+.container {
+  width: 400px;
+  height: 200px;
+  top: 0;
+  overflow: hidden;
+  border: 2px solid red;
+}
+
+.gauge-bg {
+  z-index: 1;
+  width: 200px;
+  height: 200px;
+  background-color: red;
+  /* background-color: lighten("#5df086", 15%); */
+  border-radius: 250 250 0 0;
+  border: 2px solid blue;
+}
+
+.gauge-middle {
+  z-index: 3;
+  /* position: absolute; */
+  background-color: #4d4d4d;
+  width: 400px;
+  height: calc(250px);
+  top: 75px;
+  margin-left: 75px;
+  margin-right: auto;
+  border-radius: 250px 250px 0 0;
+  border: 2px solid yellow;
+}
+
+.gauge-overlay {
+  z-index: 2;
+  /* position: absolute; */
+  background-color: #5df086;
+  width: 400px;
+  height: 200px;
+  top: 200px;
+  border-radius: 0 0 200px 200px;
+  transform-origin: center top;
+}
+
+.gauge-data {
+  z-index: 4;
+  color: #5df086;
+  /* position: absolute; */
+  width: 400px;
+  bottom: 0;
+  text-align: center;
+  font-size: 24px;
+}
+</style>  
+
+<!-- <div class="container">
+    <div class="gauge-bg"></div>
+    <div class="gauge-middle"></div>
+    <div class="gauge-overlay" :style="{ transform: rotate }"></div>
+    <div class="gauge-data">
+      <span>{{ percentage }}%</span>
+    </div>
+  </div> -->
