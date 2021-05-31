fc5d416d030568f8797654fb7df4a2e96924fee7
ravenusmc <mcuddy77@gmail.com>
2021-05-31 12:59:06 -0400

    changed design of landing page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
-    
-      <section>
-        <div class='title-div'>
-          <h1 class='title_header'>Data Tool</h1>
-        </div>
-      </section>
+    <section>
+      <div class="title-div" :style="{ filter: blurCalculation }">
+        <h1 class="title_header">Data Tool</h1>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: "Home",
+  computed: {
+    blurCalculation() {
+      return "blur(1.3px)";
+    },
+  }
 };
-
 </script>
 
 <style scoped>
-
 section {
-  background-image: url('../assets/images/landing.jpg');
+  background-image: url("../assets/images/landing.jpg");
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
-	height: 100vh;
+  height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -33,16 +33,16 @@ section {
   align-items: center;
 }
 
+.title-div {
+  background: hsla(240, 100%, 100%, 0.7);
+  padding: 20px;
+  /* filter: blur(1.3px); */
+}
+
 .title_header {
   font-size: 6rem;
   font-weight: 600;
-  letter-spacing: -.3rem;
-  color: rgb(225, 100, 0);
+  letter-spacing: -0.3rem;
+  text-transform: uppercase;
 }
-
-.title-div {
-  background-color: rgba(0, 125, 225, 0.8);
-  padding: 20px;
-}
-
 </style>
