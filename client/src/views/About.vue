b64d6688af466f84e6b7b0b7b3b9f9a6facede3a
ravenusmc <mcuddy77@gmail.com>
2021-05-31 11:04:34 -0400

    cleaned up navbar and footer

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <Navbar />
-
     <!-- Header area -->
     <div class="about_header">
       <h1 class="center font">About Me</h1>
@@ -57,20 +55,13 @@
 
     <hr />
 
-    <Footer />
   </div>
 </template>
 
 <script>
-import Navbar from "@/components/generic/Navbar.vue";
-import Footer from "@/components/generic/Footer.vue";
 
 export default {
   name: "About",
-  components: {
-    Navbar,
-    Footer,
-  },
 };
 </script>
 
--- client/src/views/TextAnalysis.vue
+++ client/src/views/TextAnalysis.vue
@@ -1,29 +1,23 @@
 <template>
   <div>
-    <Navbar />
     <Purpose />
     <section v-if="showSentimentResults">
       <hr />
       <Results />
     </section>
-    <Footer />
   </div>
 </template>
 
 <script>
-import Navbar from "@/components/generic/Navbar.vue";
 import Purpose from "@/components/text/Purpose.vue";
 import Results from "@/components/text/Results.vue";
-import Footer from "@/components/generic/Footer.vue";
 import { mapGetters } from "vuex";
 
 export default {
   name: "TextAnalysis",
   components: {
-    Navbar,
     Purpose,
     Results,
-    Footer,
   },
   computed: {
     ...mapGetters("text", ["showSentimentResults"]),
