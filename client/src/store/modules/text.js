c66d2be38f7ba6b0417614a788cb2d999bbf9293
ravenusmc <mcuddy77@gmail.com>
2021-06-10 08:04:38 -0400

    added feature to clear state when new text file is uploaded

--- client/src/store/modules/text.js
+++ client/src/store/modules/text.js
@@ -30,27 +30,53 @@ const getters = {
 
 const actions = {
 
-	handleFileUpload: ({ commit }, { formData }) => {
-		const path = 'http://localhost:5000/text_file_upload';
-		axios.post(path, formData, {
-			headers: {
-				'Content-Type': 'multipart/form-data'
-			}
-		})
-			.then((res) => {
-				console.log(res.data)
-				commit('setFileName', res.data.file_name)
-				commit('setTextFile', res.data);
-				commit('setShowSentimentResults', true);
-				commit('setSentence', res.data.sentence_and_sentiment_list[0].sentence)
-				commit('setSentenceSentiment', res.data.sentence_and_sentiment_list[0].sentiment)
-				commit('setTextLength', res.data.sentence_and_sentiment_list.length)
-				commit('setSentiment_graph_data', res.data.sentiment_graph_data)
-				res.data.word_count_chart_data.sort((a, b) => b[1] - a[1]);
-				commit('setWord_count_graph_data', res.data.word_count_chart_data)
-			})
-			.catch(error => {
-				console.log(error);
+	clearTextState: ({ commit }) => {
+		let fileName = ''
+		let textFile = {}
+		let showSentimentResults = false
+		let initalValue = 0
+		let sentence = ''
+		let sentenceSentiment = 0
+		let textLength = 0
+		let sentiment_graph_data = []
+		let word_count_graph_data = []
+		commit('setFileName', fileName)
+		commit('setTextFile', textFile);
+		commit('setShowSentimentResults', showSentimentResults);
+		commit('setInitalValue', initalValue)
+		commit('setSentence', sentence)
+		commit('setSentenceSentiment', sentenceSentiment)
+		commit('setTextLength', textLength)
+		commit('setSentiment_graph_data', sentiment_graph_data)
+		commit('setWord_count_graph_data', word_count_graph_data)
+	},
+
+	handleFileUpload: ({ commit, dispatch }, { formData }) => {
+		return Promise.all([
+			dispatch('clearTextState'),
+		])
+			.then(() => {
+				const path = 'http://localhost:5000/text_file_upload';
+				axios.post(path, formData, {
+					headers: {
+						'Content-Type': 'multipart/form-data'
+					}
+				})
+					.then((res) => {
+						console.log(res.data)
+						commit('setFileName', res.data.file_name)
+						commit('setTextFile', res.data);
+						commit('setShowSentimentResults', true);
+						commit('setSentence', res.data.sentence_and_sentiment_list[0].sentence)
+						commit('setSentenceSentiment', res.data.sentence_and_sentiment_list[0].sentiment)
+						commit('setTextLength', res.data.sentence_and_sentiment_list.length)
+						commit('setSentiment_graph_data', res.data.sentiment_graph_data)
+						res.data.word_count_chart_data.sort((a, b) => b[1] - a[1]);
+						commit('setWord_count_graph_data', res.data.word_count_chart_data)
+					})
+					.catch(error => {
+						console.log(error);
+					})
 			})
 	},
 
