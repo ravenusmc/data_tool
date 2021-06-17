38408c86f0b99b3d660518e84cad45f7d1d57f35
ravenusmc <mcuddy77@gmail.com>
2021-06-17 08:20:08 -0400

    working on profile page

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -2,13 +2,18 @@
   <div>
     <section>
       <p>Hi {{ userObject.username }}!</p>
-
+      <button
+        class="btn btn-outline-primary"
+        v-on:click="showChangePasswordArea()"
+      >
+        {{ this.changePasswordButtonText }}
+      </button>
       <form @submit="submit">
         <div>
           <h3 class="login-title center">Login</h3>
-          <p class="center redText" v-if="passwordNoMatch">
+          <!-- <p class="center redText" v-if="passwordNoMatch">
             Password Or Username Invalid
-          </p>
+          </p> -->
         </div>
 
         <div class="form-group">
@@ -31,8 +36,18 @@
             :placeholder="userObject.email"
           />
         </div>
-        <div>
-          <div class="form-group" v-if="changePasswordArea">
+        <div v-if="changePasswordArea">
+          <div class="form-group">
+            <label for="exampleInputPassword1">Original Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              v-model="original_password"
+              placeholder="Original Password"
+            />
+          </div>
+          <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
             <input
               type="password"
@@ -42,16 +57,20 @@
               placeholder="Password"
             />
           </div>
+          <div class="form-group">
+            <label for="exampleInputPassword2">Confirm Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword2"
+              v-model="password2"
+              placeholder="Confirm Password"
+            />
+          </div>
         </div>
 
         <button type="submit" class="btn btn-outline-primary">Submit</button>
       </form>
-      <button
-        class="btn btn-outline-primary"
-        v-on:click="showChangePasswordArea()"
-      >
-        {{ this.changePasswordButtonText }}
-      </button>
     </section>
   </div>
 </template>
@@ -66,6 +85,10 @@ export default {
       changePasswordArea: false,
       changePasswordButtonText: "Change Password",
       username: "",
+      email: "",
+      original_password: "",
+      password: "",
+      password2: "",
     };
   },
   computed: {
@@ -74,11 +97,23 @@ export default {
   methods: {
     submit(evt) {
       evt.preventDefault();
-      const payload = {
-        username: this.username,
-        password: this.password,
-      };
-      this.$store.dispatch("common/login", { payload });
+      console.log(this.$store.getters['session/userObject'])
+      if (this.username == "") {
+        this.username = this.$store.getters['session/userObject'].username
+      } else if (this.password != this.password2) {
+        alert("Passwords are not the same");
+      } else if (this.password.length < 6) {
+        alert("Password must be at least 6 characters long");
+      } else {
+        const payload = {
+          username: this.username,
+          email: this.email,
+          original_password: this.original_password,
+          password: this.password,
+        };
+        console.log(payload);
+        // this.$store.dispatch("common/login", { payload });
+      }
     },
     showChangePasswordArea() {
       if (this.changePasswordArea == true) {
@@ -88,7 +123,6 @@ export default {
         this.changePasswordButtonText = "Hide Change Password";
         this.changePasswordArea = true;
       }
-      console.log("hi");
     },
   },
 };
