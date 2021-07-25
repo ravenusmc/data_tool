02c888064131474e78edc02776c11456c21a5363
ravenusmc <mcuddy77@gmail.com>
2021-07-25 11:41:12 -0400

    adding ability to select format of chart

--- client/src/components/data/datacomponents/FilterArea.vue
+++ client/src/components/data/datacomponents/FilterArea.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
-
+    <section>
+      <div>
+        <input type="radio" id="one" value="BarChart" v-model="graphType" />
+        <label for="one">Bar Chart</label>
+        <br />
+        <input
+          @change="test()"
+          type="radio"
+          id="two"
+          value="PieChart"
+          v-model="graphType"
+        />
+        <label for="two">Pie Chart</label>
+        <br />
+        <span>Picked: {{ graphType }}</span>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   name: "FilterArea",
+  data() {
+    return {
+      graphType: "",
+    };
+  }, // End of Data
+  methods: {
+    test() {
+      console.log("hi");
+    },
+  },
 };
 </script>
 
