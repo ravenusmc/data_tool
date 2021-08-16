2cd470415488b58bf04f387a689082d7d562ba4a
ravenusmc <mcuddy77@gmail.com>
2021-08-16 18:56:30 -0400

    working on building sorting algorithm"

--- server/data_helper.py
+++ server/data_helper.py
@@ -7,23 +7,33 @@ import pandas as pd
 
 class Data_Helper():
 
-    def get_unique_values_x_axis(self, data_file, post_data):
-        return data_file[post_data['payload']['xAxisValue']].unique()
+	def get_unique_values_x_axis(self, data_file, post_data):
+		return data_file[post_data['payload']['xAxisValue']].unique()
 
-    def get_unique_values_y_axis(self, data_file, post_data):
-        return data_file[post_data['payload']['yAxisValue']].unique()
+	def get_unique_values_y_axis(self, data_file, post_data):
+		return data_file[post_data['payload']['yAxisValue']].unique()
 
-    def get_length_unique_values(self, unique_values):
-        return len(unique_values)
+	def get_length_unique_values(self, unique_values):
+		return len(unique_values)
 
-    def sort_graph_data(self, graph_data):
-        # print(type(graph_data))
-        for value in graph_data[1:]:
+	def get_list_total(self, value):
+		current_value_sum = 0
+		for v in value[1:]:
+			if isinstance(v, int):
+				v = int(v)
+				current_value_sum = v + current_value_sum
+		return current_value_sum
+
+	def sort_graph_data(self, graph_data):
+		outer_loop_count = 1
+		for value in graph_data[1:]:
 			current_data_list = value
-            current_value_sum = 0
-            for v in value[1:]:
-                v = int(v)
-                current_value_sum = v + current_value_sum
-            print(value)
-            print(current_value_sum)
-            input()
+			current_value_sum_first = self.get_list_total(value)
+			inner_loop_count = 2
+			for v in graph_data[2:]:
+				current_value_sum_second = self.get_list_total(v)
+				if current_value_sum_first > current_value_sum_second:
+					graph_data[outer_loop_count], graph_data[current_value_sum_second] = graph_data[
+						current_value_sum_second], graph_data[outer_loop_count]
+		print(graph_data)
+
