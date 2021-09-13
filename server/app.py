79e5eaeb4d9ffd1d22e6a0b7784bddba3548e234
ravenusmc <mcuddy77@gmail.com>
2021-09-13 09:48:04 -0400

    changing graphing code on server side

--- server/app.py
+++ server/app.py
@@ -163,6 +163,7 @@ def build_data_graph():
         # Checking for unique Values - y axis
         unique_values_y_axis = data_helper_obj.get_unique_values_y_axis(
             data_file, post_data)
+        print(unique_values_y_axis)
         # Getting graph data
         graph_data = data_object.get_column_data_for_graph(
             data_file, post_data, unique_values)
@@ -175,11 +176,25 @@ def build_data_graph():
         data_graph_information['show_graph'] = True
         data_graph_information['show_chart_controls'] = True
         if unique_values_length_x_axis > 8:
+            if post_data['payload']['aggregateValue']:
+                graph_data = data_object.get_column_data_for_graph_aggregate(
+                    data_file, post_data, unique_values, unique_values_y_axis)
+                graph_data = data_helper_obj.sort_graph_data(graph_data)
+                data_graph_information['graph_data'] = graph_data
+                data_graph_information['show_user_warning'] = False
+                data_graph_information['show_graph'] = True
+                data_graph_information['show_chart_controls'] = True
             graph_data = graph_data[0:len(graph_data)-1]
             data_graph_information['show_user_warning'] = True
             data_graph_information['graph_data'] = graph_data
+            return jsonify(data_graph_information)
+        if post_data['payload']['uniqueValue'] == 'true':
+            unique_values_y_axis = data_helper_obj.get_unique_values_y_axis(
+                    data_file, post_data)
+            graph_data = data_object.get_column_data_for_graph_unique_y_values(
+                    data_file, post_data, unique_values, unique_values_y_axis)
+            return jsonify(data_graph_information)
         if post_data['payload']['aggregateValue']:
-            print('here')
             graph_data = data_object.get_column_data_for_graph_aggregate(
                 data_file, post_data, unique_values, unique_values_y_axis)
             graph_data = data_helper_obj.sort_graph_data(graph_data)
