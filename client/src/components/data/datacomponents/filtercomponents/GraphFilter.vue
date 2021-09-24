9e2b24bbeb17e47ae881cb7a36dc95d66781010f
ravenusmc <mcuddy77@gmail.com>
2021-09-24 13:58:55 -0400

    broke up filterArea.vue into two components

---
+++ client/src/components/data/datacomponents/filtercomponents/GraphFilter.vue
@@ -0,0 +1,214 @@
+<template>
+  <div>
+    <section v-if="chartControls" class="chart_controls">
+      <!-- change graph Title div -->
+      <div>
+        <input
+          type="text"
+          id="title"
+          placeholder="Chart Title"
+          v-model="title"
+        />
+        <button
+          type="submit"
+          v-on:click="makeChartTitle()"
+          class="btn btn-outline-primary"
+        >
+          Change Chart Title
+        </button>
+      </div>
+      <!-- End change graph Title div -->
+      <!-- change graph color div -->
+      <div>
+        <label class="font">Color:</label>&nbsp;
+        <select v-model="color" name="colot">
+          <option v-for="color in colors" v-bind:key="color" :value="color">
+            {{ color }}
+          </option>
+        </select>
+        <button
+          type="submit"
+          v-on:click="changeChartColor()"
+          class="btn btn-outline-primary"
+        >
+          Change Chart Color
+        </button>
+      </div>
+      <div>
+        <label for="two"># of columns for X-Axis:</label>
+        <div class="tooltip">
+          <span class="tooltiptext">Tooltip text</span>
+        </div>
+        <input type="number" id="title" v-model="xAxisCountNumber" />
+        <button
+          type="submit"
+          v-on:click="changeXAxisColumnNumber()"
+          class="btn btn-outline-primary"
+          v-tooltip.top="msg"
+        >
+          Change X Axis Column Number
+        </button>
+      </div>
+      <div>
+        <label for="two"># of columns for Y-Axis:</label>
+        <div class="tooltip">
+          <span class="tooltiptext">Tooltip text</span>
+        </div>
+        <input type="number" id="title" v-model="yAxisCountNumber" />
+        <button
+          type="submit"
+          v-on:click="changeYAxisColumnNumber()"
+          class="btn btn-outline-primary"
+          v-tooltip.top="yAxisColumnNumberMSG"
+        >
+          Change Y Axis Column Number
+        </button>
+      </div>
+      <div>
+        <label for="two">H-Axis Label:</label>
+        <input type="text" id="title" v-model="hAxisName" />
+        <button
+          type="submit"
+          v-on:click="changeHAxisTitle()"
+          class="btn btn-outline-primary"
+        >
+          Change H-Axis Label
+        </button>
+      </div>
+      <div>
+        <label for="two">V-Axis Label:</label>
+        <input type="text" id="title" v-model="vAxisName" />
+        <button
+          type="submit"
+          v-on:click="changeVAxisTitle()"
+          class="btn btn-outline-primary"
+        >
+          Change V-Axis Label
+        </button>
+      </div>
+    </section>
+    <!-- End change graph color div -->
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "GraphFilter",
+  data() {
+    return {
+      xAxisCountNumber: 5,
+      yAxisCountNumber: 4,
+      hAxisName: "",
+      vAxisName: "",
+      title: "",
+      color: "blue",
+      colors: ["blue", "red", "black", "orange"],
+      aggregateValueChecked: false,
+      chartControls: false,
+      // Need to fix this message...as well as do this better.
+      msg:
+        "Here you can select" +
+        "</br>" +
+        "how many columns you" +
+        "</br>" +
+        "want on the x-axis.",
+      yAxisColumnNumberMSG:
+        "Here you can select" +
+        "</br>" +
+        "how many columns you" +
+        "</br>" +
+        "want on the y-axis" +
+        "</br>" +
+        "when aggregate is selected.",
+    };
+  }, // End of Data
+  computed: {
+    ...mapGetters("data", [
+      "showGraph", // May not need this
+      "showChartControls",
+      "hideControlsBasedOnAggregateValueSelected",
+    ]),
+  },
+  methods: {
+    ...mapActions("data", [
+      "changeGraphType",
+      "changeUniqueValue",
+      "changeChartTitle",
+      "changeChartColorAction",
+      "changeAggregateValue",
+      "changeGraphData",
+      "fetchGraph",
+      "changeHAxisName",
+      "changeVAxisName",
+    ]),
+    graphTypeSelected() {
+      // This needs to be fixed...don't need payload here
+      const payload = {
+        graphType: this.graphType,
+      };
+      this.changeGraphType(payload);
+    },
+    uniqueValueSelected() {
+      this.changeUniqueValue(this.uniqueValue);
+    },
+    aggregateValueSelected() {
+      this.changeAggregateValue(this.aggregateValueChecked);
+    },
+    makeChartTitle() {
+      this.changeChartTitle(this.title);
+    },
+    changeChartColor() {
+      this.changeChartColorAction(this.color);
+    },
+    setShowChartControls() {
+      this.chartControls = this.showChartControls;
+    },
+    changeXAxisColumnNumber() {
+      const payload = {
+        xAxisCountNumber: this.xAxisCountNumber,
+        fromButton: true,
+      };
+      this.changeGraphData(payload);
+    },
+    changeYAxisColumnNumber() {
+      if (this.$store.getters["data/xAxisValue"] == "") {
+        alert("Please put a column in for the x-value");
+      } else if (this.$store.getters["data/yAxisValue"] == "") {
+        alert("Please put a column in for the y-value");
+      } else {
+        const payload = {
+          fileName: this.$store.getters["data/fileName"],
+          xAxisValue: this.$store.getters["data/xAxisValue"],
+          yAxisValue: this.$store.getters["data/yAxisValue"],
+          yAxisValue: this.$store.getters["data/yAxisValue"],
+          uniqueValue: this.$store.getters["data/uniqueValue"],
+          aggregateValue: this.$store.getters["data/aggregateValue"],
+          yAxisCountNumber: this.yAxisCountNumber,
+        };
+        this.fetchGraph({ payload });
+      }
+    },
+    changeHAxisTitle() {
+      this.changeHAxisName(this.hAxisName);
+    },
+    changeVAxisTitle() {
+      this.changeVAxisName(this.vAxisName);
+    },
+  },
+  watch: {
+    showChartControls: {
+      handler(value) {
+        if (value) {
+          this.setShowChartControls();
+        }
+      },
+      immediate: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
