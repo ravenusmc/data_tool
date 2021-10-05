80a0cc0a61f55b3369172594fe6df9f989a1ae96
ravenusmc <mcuddy77@gmail.com>
2021-10-05 07:49:52 -0400

    made aggragate value component

--- client/src/components/data/datacomponents/filtercomponents/InitialFilter.vue
+++ client/src/components/data/datacomponents/filtercomponents/InitialFilter.vue
@@ -3,59 +3,22 @@
     <div class="initial-filter-area">
       <ChartTypes />
       <UniqueValues />
-      <div class="filter-fix">
-        <h6>Use Aggregate Value</h6>
-        <input
-          @change="aggregateValueSelected()"
-          type="checkbox"
-          id="checkbox"
-          value="true"
-          v-model="aggregateValueChecked"
-        />
-        <label for="checkbox">Aggregate Value Y Axis</label>
-      </div>
+      <AggValue />
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
 import ChartTypes from "@/components/data/datacomponents/filtercomponents/filterparts/ChartType.vue";
 import UniqueValues from "@/components/data/datacomponents/filtercomponents/filterparts/UniqueValues.vue";
+import AggValue from "@/components/data/datacomponents/filtercomponents/filterparts/AggValue.vue";
 
 export default {
   name: "InitialFilter",
   components: {
     ChartTypes,
     UniqueValues,
-  },
-  data() {
-    return {
-      graphType: "",
-      uniqueValue: "",
-      aggregateValueChecked: false,
-      chartControls: false,
-    };
-  }, // End of Data
-  methods: {
-    ...mapActions("data", [
-      "changeGraphType",
-      "changeUniqueValue",
-      "changeAggregateValue",
-    ]),
-    graphTypeSelected() {
-      // This needs to be fixed...don't need payload here
-      const payload = {
-        graphType: this.graphType,
-      };
-      this.changeGraphType(payload);
-    },
-    uniqueValueSelected() {
-      this.changeUniqueValue(this.uniqueValue);
-    },
-    aggregateValueSelected() {
-      this.changeAggregateValue(this.aggregateValueChecked);
-    },
+    AggValue,
   },
 };
 </script>
@@ -64,9 +27,5 @@ export default {
 .initial-filter-area {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(150px, 250px));
-}
-
-.filter-fix {
-  margin: 10px;
 }
 </style>
---
+++ client/src/components/data/datacomponents/filtercomponents/filterparts/AggValue.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="filter-fix">
+      <h6>Use Aggregate Value</h6>
+      <input
+        @change="aggregateValueSelected()"
+        type="checkbox"
+        id="checkbox"
+        value="true"
+        v-model="aggregateValueChecked"
+      />
+      <label for="checkbox">Aggregate Value Y Axis</label>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "AggValue",
+  data() {
+    return {
+      aggregateValueChecked: false,
+      chartControls: false,
+    };
+  }, // End of Data
+  methods: {
+    ...mapActions("data", ["changeAggregateValue"]),
+    aggregateValueSelected() {
+      this.changeAggregateValue(this.aggregateValueChecked);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.filter-fix {
+  margin: 10px;
+}
+</style>
